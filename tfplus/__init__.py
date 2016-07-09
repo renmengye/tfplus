2af12734d6781bd2b50e90aa683968f48d7584cd
Mengye Ren <renmengye@gmail.com>
2016-07-09 18:20:56 -0400

    data

--- tfplus/__init__.py
+++ tfplus/__init__.py
@@ -1,3 +1,5 @@
 from expr import experiment, runner
+import data
+import nn
 from utils import cmd_args
 from utils.cmd_args import init
--- tfplus/data/__init__.py
+++ tfplus/data/__init__.py
@@ -1,1 +1,2 @@
 from data_provider import get_factory, create_from_main, create
+from data_provider import DataProvider
