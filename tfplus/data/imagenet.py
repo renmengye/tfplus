f3aa7658794ac4f4cdfd46063c79a4d3e72ced07
Mengye Ren <renmengye@gmail.com>
2016-07-15 00:21:17 -0400

    preproc

--- tfplus/data/imagenet.py
+++ tfplus/data/imagenet.py
@@ -27,12 +27,7 @@ class ImageNetDataProvider(tfplus.data.DataProvider):
         self._folder = folder
         self._img_ids = None
         self._labels = None
-        # self._random = np.random.RandomState(2)
-        # self._crop = crop
         self._mode = mode
-        # self._rnd_resize = rnd_resize
-        # self._image_in, self._image_out = self.build_trans_graph()
-        # self._sess = tf.Session()
         self._rnd_proc = ImagePreprocessor(
             rnd_hflip=True, rnd_colour=True, rnd_resize=[256, 480], resize=256,
             crop=224)
@@ -87,24 +82,9 @@ class ImageNetDataProvider(tfplus.data.DataProvider):
     def get_size(self):
         return len(self.img_ids)
 
-    # def build_trans_graph(self):
-    #     device = '/cpu:0'
-    #     with tf.device(device):
-    #         image_in = tf.placeholder('float', [None, None, 3])
-    #         image_out = image_in
-    #         image_out = tf.image.random_flip_left_right(image_out)
-    #         image_out = tf.image.random_brightness(
-    #             image_out, max_delta=32. / 255.)
-    #         image_out = tf.image.random_saturation(
-    #             image_out, lower=0.5, upper=1.5)
-    #         image_out = tf.image.random_hue(image_out, max_delta=0.2)
-    #         image_out = tf.image.random_contrast(
-    #             image_out, lower=0.5, upper=1.5)
-    #     return image_in, image_out
-
     def get_batch_idx(self, idx, **kwargs):
         start_time = time.time()
-        x = np.zeros([len(idx), self._crop, self._crop, 3], dtype='float32')
+        x = None
         y_gt = np.zeros([len(idx), 1000], dtype='float32')
         for kk, ii in enumerate(idx):
             if self.split == 'train':
@@ -116,6 +96,9 @@ class ImageNetDataProvider(tfplus.data.DataProvider):
             x_ = cv2.imread(img_fname)
             rnd = self._mode == 'train'
             x_ = self._rnd_proc.process(x_, rnd=rnd)
+            if x = None:
+                x = np.zeros([len(idx), x_.shape[0], x_.shape[1], x_.shape[2]],
+                             dtype='float32')
             x[kk] = x_
             y_gt[kk, self.labels[ii]] = 1.0
         results = {
