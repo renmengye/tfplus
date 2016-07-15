13149cbf5d16656cb1dcde5552bd8103ad6165a1
Mengye Ren <renmengye@gmail.com>
2016-07-15 00:13:17 -0400

    preproc

--- tfplus/data/img_preproc.py
+++ tfplus/data/img_preproc.py
@@ -14,6 +14,7 @@ class ImagePreprocessor(object):
         self._crop = crop
         self._rnd_colour = rnd_colour
         self._image_in, self._image_out = self.build_colour_graph()
+        self.redraw()
         self._sess = tf.Session()
         pass
 
@@ -33,27 +34,33 @@ class ImagePreprocessor(object):
             image_out = tf.clip_by_value(image_out, 0.0, 1.0)
         return image_in, image_out
 
+    def redraw():
+        self.siz = int(self._random.uniform(
+            self._rnd_resize[0], self._rnd_resize[1]))
+        self.offset = []
+        self.offset[0] = int(self._random.uniform(0.0, siz - self._crop))
+        self.offset[1] = int(self._random.uniform(0.0, siz - self._crop))
+        self.hflip = bool(self._random.uniform(0, 1))
+        pass
+
     def process(self, image, rnd=True, redraw=True):
         """Process the images.
 
         redraw: Whether to redraw random numbers used for random cropping, and
         horizontal flipping. Random colours have to be redrawn.
         """
+        if redraw:
+            self.redraw()
+
         if rnd:
-            siz = int(self._random.uniform(
-                self._rnd_resize[0], self._rnd_resize[1]))
-            offset = [0, 0]
-            offset[0] = int(self._random.uniform(0.0, siz - self._crop))
-            offset[1] = int(self._random.uniform(0.0, siz - self._crop))
-            if self._rnd_hflip:
-                hflip = bool(self._random.uniform(0, 1))
-            else:
-                hflip = False
+            siz = self.siz
+            offset = self.offset
+            hflip = self.hflip and self._rnd_hflip
         else:
             siz = self._resize
             offset = (self._rnd_resize[0] - self._crop) / 2
             offset = [offset, offset]
-            pass
+            hflip = False
 
         image = cv2.resize(image, (siz, siz), interpolation=cv2.INTER_CUBIC)
         image = image[offset[0]: self._crop + offset[0],
