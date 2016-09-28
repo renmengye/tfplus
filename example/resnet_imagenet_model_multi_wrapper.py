d3688a8084f33f837e9312ffb8d3bd03bd5387c6
Mengye Ren <mren@twitter.com>
2016-09-27 23:16:52 -0400

    all

--- example/resnet_imagenet_model_multi_wrapper.py
+++ example/resnet_imagenet_model_multi_wrapper.py
@@ -101,25 +101,27 @@ class ResNetImageNetModelMultiWrapper(tfplus.nn.ContainerModel):
             # Note that each grad_and_vars looks like the following:
             #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
             grads = []
-            for g, _ in grad_and_vars:
-                # Add 0 dimension to the gradients to represent the tower.
-                if g is not None:
+            if grad_and_vars[0][0] is not None
+                for g, _ in grad_and_vars:
+                    # Add 0 dimension to the gradients to represent the tower.
                     expanded_g = tf.expand_dims(g, 0)
 
-                # Append on a 'tower' dimension which we will average over
-                # below.
-                grads.append(expanded_g)
+                    # Append on a 'tower' dimension which we will average over
+                    # below.
+                    grads.append(expanded_g)
 
-            # Average over the 'tower' dimension.
-            grad = tf.concat(0, grads)
-            grad = tf.reduce_mean(grad, 0)
+                # Average over the 'tower' dimension.
+                grad = tf.concat(0, grads)
+                grad = tf.reduce_mean(grad, 0)
 
-            # Keep in mind that the Variables are redundant because they are shared
-            # across towers. So .. we will just return the first tower's pointer to
-            # the Variable.
-            v = grad_and_vars[0][1]
-            grad_and_var = (grad, v)
-            average_grads.append(grad_and_var)
+                # Keep in mind that the Variables are redundant because they are shared
+                # across towers. So .. we will just return the first tower's pointer to
+                # the Variable.
+                v = grad_and_vars[0][1]
+                grad_and_var = (grad, v)
+                average_grads.append(grad_and_var)
+            else:
+                average_grads.append(grad_and_vars[0])
         return average_grads
 
     def build_optim(self, loss):
